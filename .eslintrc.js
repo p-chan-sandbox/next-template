dd84a9ffb85933bd929a7a3a1dedfbe61ab5e5a9
P-Chan <hello@0x50.io>
2023-12-30 20:39:44 +0900

    feat: remove tailwindcss

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,4 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
-  extends: [
-    '@stardust-configs/eslint-config/presets/next',
-    '@stardust-configs/eslint-config/configs/tailwindcss',
-    '@stardust-configs/eslint-config/configs/prettier',
-  ],
+  extends: ['@stardust-configs/eslint-config/presets/next', '@stardust-configs/eslint-config/configs/prettier'],
 }
